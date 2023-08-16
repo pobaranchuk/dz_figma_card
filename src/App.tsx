902472202abac82743fb165534408951c3951770
pobaranchuk <you@example.com>
2023-08-16 18:26:17 +0200

    New commit

--- src/App.tsx
+++ src/App.tsx
@@ -5,6 +5,7 @@ import {MainPictureStyled} from "./components/MainPicture.styled";
 import mainImg from "./assets/images/my_picture.jpg"
 import {HeaderStyled} from "./components/Header.styled";
 import {ParagraphStyled} from "./components/Paragraph.styled";
+import {ButtonStyled} from "./components/Button.styled";
 
 function App() {
     return (
@@ -13,11 +14,10 @@ function App() {
                 <MainPictureStyled src={mainImg}/>
                 <HeaderStyled>Headline</HeaderStyled>
                 <ParagraphStyled>Faucibus. Faucibus. Sit sit sapien sit tempusrisu ut. Sit molestie ornare in venen.</ParagraphStyled>
-
+                <span><ButtonStyled buttonType ={"secondary"}>See more</ButtonStyled></span>
+                <span><ButtonStyled buttonType={"primary"}>Save</ButtonStyled></span>
             </MainCardStyled>
-
         </div>
     );
 }
-
 export default App;
---
+++ src/components/Button.styled.tsx
@@ -0,0 +1,34 @@
+import styled, {css} from "styled-components";
+
+type ButtonStyledPropsType = {
+    buttonType?: "primary" | "secondary"
+}
+
+export const ButtonStyled = styled.button<ButtonStyledPropsType>`
+  border-radius: 5px;
+  background: #4E71FE;
+  width: 86px;
+  height: 30px;
+  border: 2px solid #4E71FE;
+  margin-left: 20px;
+  margin-bottom: 22px;
+  float: left;
+  
+  ${props => props.buttonType === "primary" && css<ButtonStyledPropsType>`
+    background-color: transparent;
+    color: #4E71FE;
+    font-family: Inter;
+    font-size: 12px;
+    font-style: normal;
+    font-weight: 700;
+  
+  `}  ${props => props.buttonType === "secondary" && css<ButtonStyledPropsType>`
+    background-color: #4E71FE;
+    color: #FFFFFF;
+    font-family: Inter;
+    font-size: 12px;
+    font-style: normal;
+    font-weight: 700;
+  
+  `}
+`
